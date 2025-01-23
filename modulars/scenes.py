a466ba85c3c7222143f5d2f162fde223838c37cc
RoNiN-shadow <138571922+RoNiN-shadow@users.noreply.github.com>
2025-01-23 20:01:32 +0200

    Update scenes.py
    added more classes like Submenu and Canvas.
    
    New FEATURE: you can now choose the size of **brush**

--- modulars/scenes.py
+++ modulars/scenes.py
@@ -1,58 +1,98 @@
 import tkinter as tk
 
+
 class Paint(tk.Frame):
-	def __init__(self, master):
-		super().__init__(master)
-		
-		self.canvas = tk.Canvas(self, bg="white")
-		self.canvas.pack(fill=tk.BOTH, expand=True)
+    def __init__(self, master):
+        super().__init__(master)
 
-		
-		self.canvas.bind("<B1-Motion>", self.draw_pixel)
+        self.canvas = PaintCanvas(self, bg="white")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
 
-		self.selected = tk.IntVar(value=1)
-
-		
-		self.choose_color_menu = tk.Menu(self, tearoff=0)
-		color_menu = tk.Menu(self.choose_color_menu, tearoff=0)
-		color_menu.add_radiobutton(label="Чорний", command=self.set_black_color, value=1, variable=self.selected)
-		
-		color_menu.add_radiobutton(label="Синій", command=self.set_blue_color, value=2, variable=self.selected)
-		
-		color_menu.add_radiobutton(label="Жовтий", command=self.set_yellow_color, value=3, variable=self.selected)
-		color_menu.add_command(label="Очистити", command=self.set_white_color)
-	
-			
-		self.choose_color_menu.add_cascade(label="Колір", menu=color_menu)
-		
-		self.master.config(menu=self.choose_color_menu)
-
-	def draw_pixel(self, event):
-		selected = self.selected.get()
-		if selected ==1:
-			color = "black"
-		elif selected == 2:
-			color = "blue"
-		elif selected ==3:
-			color = "yellow"
-		else:
-			color = "black"
-		x,y = event.x, event.y
-		self.canvas.create_rectangle(x,y,x+1,y+1, fill=color, width=2, outline=color)
+        self.canvas.bind("<B1-Motion>",
+                         lambda event: self.canvas.draw_pixel(event, sub_color_menu.selected_variable.get(), brash_size_menu.selected_variable.get()))
 
 
-	def set_black_color(self):
-		self.selected.set(1)
-		self.update_idletasks()
+        self.main_menu = tk.Menu(self, tearoff=0)
 
-	def set_white_color(self):
-		self.canvas.delete("all")
 
-	def set_blue_color(self):
-		self.selected.set(2)
 
-		self.update_idletasks()
-	def set_yellow_color(self):
-		self.selected.set(3)
 
-		self.update_idletasks()
+
+
+
+
+        sub_color_menu = Submenu(self.main_menu, tearoff=0)
+        sub_color_menu.add_radiobutton(label="Чорний", command=lambda: sub_color_menu.set_variable(1), value=1,
+                                       variable=sub_color_menu.selected_variable)
+        sub_color_menu.add_radiobutton(label="Синій", command=lambda: sub_color_menu.set_variable(2), value=2,
+                                       variable=sub_color_menu.selected_variable)
+        sub_color_menu.add_radiobutton(label="Жовтий", command=lambda: sub_color_menu.set_variable(3), value=3,
+                                       variable=sub_color_menu.selected_variable)
+        sub_color_menu.add_command(label="Очистити", command=self.set_clean_all)
+
+        self.main_menu.add_cascade(label="Колір", menu=sub_color_menu)
+
+        brash_size_menu = Submenu(self.main_menu, tearoff=0)
+        brash_size_menu.add_radiobutton(label="1 піксель", command=lambda: brash_size_menu.set_variable(1), value=1,
+                                        variable=brash_size_menu.selected_variable)
+        brash_size_menu.add_radiobutton(label="10 піксель", command=lambda: brash_size_menu.set_variable(2), value=2,
+                                        variable=brash_size_menu.selected_variable)
+        brash_size_menu.add_radiobutton(label="25 піксель", command=lambda: brash_size_menu.set_variable(3), value=3,
+                                        variable=brash_size_menu.selected_variable)
+        brash_size_menu.add_radiobutton(label="50 піксель", command=lambda: brash_size_menu.set_variable(4), value=4,
+                                        variable=brash_size_menu.selected_variable)
+
+        self.main_menu.add_cascade(label="Розмір", menu=brash_size_menu)
+
+        self.master.config(menu=self.main_menu)
+
+    def set_clean_all(self):
+        self.canvas.delete("all")
+
+
+
+
+
+class Submenu(tk.Menu):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.selected_variable: tk.IntVar = tk.IntVar(value=1)
+
+    def set_variable(self, new_value):
+        self.selected_variable.set(new_value)
+        self.update_idletasks()
+
+
+class PaintCanvas(tk.Canvas):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+    def draw_pixel(self, event, color_selected: int, brash_selected: int):
+
+        if color_selected == 1:
+            color = "black"
+        elif color_selected == 2:
+            color = "blue"
+        elif color_selected == 3:
+            color = "yellow"
+        else:
+            color = "black"
+
+        if brash_selected == 1:
+            size = 1
+        elif brash_selected == 2:
+            size = 10
+        elif brash_selected == 3:
+            size = 25
+        elif brash_selected == 4:
+            size = 50
+        else:
+            size = 1
+
+        x, y = event.x, event.y
+        self.create_rectangle(x, y, x, y, fill=color, width=size, outline=color)
+
+
+
+
